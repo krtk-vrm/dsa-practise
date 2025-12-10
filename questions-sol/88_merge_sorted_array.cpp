776ccc34420a10c257f6a085453c6040ebda32ad
krtk_vrm <kartik2104verma@gmail.com>
2025-12-10 07:52:19 +0000

    Solved 88. Merge sorted arrays

---
+++ questions-sol/88_merge_sorted_array.cpp
@@ -0,0 +1,23 @@
+/*Approach - as the arrays are already sorted and we have to moerge in nums1, i will start from 
+last element from both arrays and then put it in nums1.
+TC is O(m+n)
+SC is o(1)
+
+*/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
