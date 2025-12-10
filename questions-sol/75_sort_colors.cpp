59d1ccb2e1e1877dfeddc6795e7beecded885cb9
krtk_vrm <kartik2104verma@gmail.com>
2025-12-10 17:54:02 +0000

    Optimise 75. sort colors by using 3 pointers

--- questions-sol/75_sort_colors.cpp
+++ questions-sol/75_sort_colors.cpp
@@ -1,4 +1,5 @@
-/*Approach - we can sort all zeros at left side and 2s at right
+/*Approach - using 3 pointer technique, p1 for 0s at left, p2 for traversing in the middle
+p3 for 2s at right.
 TC is O(n)
 SC is O(1)
 */
@@ -7,32 +8,20 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int p1 = 0 , p2 = 1;
-        while(p2<n){
-            if(nums[p1]==0) {
-                p1++;
-            }
-            if(p2==p1)p2++;
-            if(p1>=n || p2>=n)break;
-            if(!nums[p2]){
+        int p1 = 0 , p2 = 0;
+        int p3 = n-1;
+
+        while(p2<=p3){
+            if(nums[p2]==0){
                 swap(nums[p1], nums[p2]);
                 p1++;
+                p2++;
             }
-            p2++;
-        }
-        p1 = n-1;
-        p2 = n-2;
-        while(p2>=0){
-            if(nums[p1]==2){
-                p1--;
+            else if(nums[p2]==1) p2++;
+            else{
+                swap(nums[p3],nums[p2]);
+                p3--;
             }
-            if(p1==p2)p2--;
-            if(p1<0 || p2<0)break;
-            if(nums[p2]==2){
-                swap(nums[p1],nums[p2]);
-                p1--;
-            }
-            p2--;
         }
     }
 };
