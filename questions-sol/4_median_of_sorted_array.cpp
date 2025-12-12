6915d041b8bb90fe7d48be65cb0e03a94ecf8210
krtk_vrm <kartik2104verma@gmail.com>
2025-12-12 17:18:50 +0000

    Other approach to solve 4. Median of sorted array

--- questions-sol/4_median_of_sorted_array.cpp
+++ questions-sol/4_median_of_sorted_array.cpp
@@ -1,17 +1,36 @@
-/*Approach - Lazy approach using stl in c++ and then returning the middle element;
+/*Approach - build an array by merging the arrays in out array and then return the median of it;
 TC is O(m+n)
 SC is O(m+n)
 */
-#pragma GCC optimize("Ofast")
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> out;
-        merge(nums1.begin(),nums1.end(), nums2.begin(), nums2.end(), back_inserter(out));
-        int n = out.size();
-        if(n%2!=0)return out[n/2];
-        else return (1.0*out[n/2]+1.0*out[n/2-1])/2;
-        
-
+        vector<double> out;
+        int n = nums1.size();
+        int m = nums2.size();
+        int p1 = 0, p2 = 0;
+        while(p1<n && p2 < m) {
+            if (nums1[p1] > nums2[p2]) {
+                out.push_back(nums2[p2]);
+                p2++;
+            }
+            else{
+                out.push_back(nums1[p1]);
+                p1++;
+            }
+        }
+        while(p1<n){
+            out.push_back(nums1[p1]);
+            p1++;
+        }
+        while(p2<m){
+            out.push_back(nums2[p2]);
+            p2++;
+        }
+        if ((n + m) % 2 != 0)
+            return out[(n + m) / 2];
+        if ((n + m) % 2 == 0)
+            return 0.5 * out[(n + m) / 2 ] + 0.5 * out[-1 + (n + m) / 2];
+        return 0;
     }
 };
