89eadcd4f464f1a7f91c85b2aea13b0d51193530
krtk_vrm <kartik2104verma@gmail.com>
2025-12-19 08:46:24 +0000

    Formatted the merge sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,34 +1,44 @@
 // divide and merge
-void merge(vector<int>& arr, int low,int  mid,int  high){
+void merge(vector<int> &arr, int low, int mid, int high)
+{
     vector<int> temp;
     int left = low;
-    int right = mid+1;
-    while(left<=mid&& right <= high){
-        if(arr[left]<=arr[right]){
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
             temp.push_back(arr[left]);
             left++;
-        }else{
+        }
+        else
+        {
             temp.push_back(right);
             right++;
         }
     }
-    while(left<=mid){
+    while (left <= mid)
+    {
         temp.push_back(arr[left]);
         left++;
     }
-    while(right<=high){
+    while (right <= high)
+    {
         temp.push_back(arr[right]);
         right++;
     }
-    for(int i = low; i <= high ; i++){
-        arr[i] = temp[i-low];
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
     }
 }
-void merge_sort(vector<int>& arr, int low, int high){
-    if(low>=high) return;
+void merge_sort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
 
-    int mid = (low+high)/2;
+    int mid = (low + high) / 2;
     merge_sort(arr, low, mid);
-    merge_sort(arr, mid+1, high);
-    merge(arr,low, mid, high );
+    merge_sort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
 }
