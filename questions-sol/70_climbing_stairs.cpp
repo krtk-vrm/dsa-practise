b7a33ba8889319b321ba534c60ce043ba51b9f77
krtk_vrm <kartik2104verma@gmail.com>
2026-01-04 17:11:18 +0000

    Solved 70. Climbing Stairs

---
+++ questions-sol/70_climbing_stairs.cpp
@@ -0,0 +1,16 @@
+/*Approach - Dynamic Programming
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1 = 1, prev2 = 1;
+        for(int i = 2 ; i <= n; i++){
+            int curr = prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
