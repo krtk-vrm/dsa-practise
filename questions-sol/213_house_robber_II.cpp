b0a34db070121e603c1a66a7bd5cda9a1eb17dfb
krtk_vrm <kartik2104verma@gmail.com>
2026-01-04 17:57:48 +0000

    Solved 213. House robber II

---
+++ questions-sol/213_house_robber_II.cpp
@@ -0,0 +1,23 @@
+/*Approach - Dynamic Programming
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+public:
+    int ro(vector<int>&nums,int start, int end){
+        int n = end-start+1;
+        if(n==1)return nums[start];
+        vector<int> dp(n);
+        dp[0]=nums[start];
+        dp[1]=max(nums[start],nums[start+1]);
+        for(int i = 2 ; i < n ; i++){
+            dp[i]=max(dp[i-1],nums[i+start]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)return nums[0];
+        return max(ro(nums,0,n-2),ro(nums,1,n-1));
+    }
+};
