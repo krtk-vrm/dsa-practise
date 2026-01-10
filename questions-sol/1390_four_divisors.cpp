a517e2e0270df07eaf924ceeb3092ed3117033f8
krtk_vrm <kartik2104verma@gmail.com>
2026-01-10 16:14:24 +0000

    Mathematical solution for 1390. Four divisors

--- questions-sol/1390_four_divisors.cpp
+++ questions-sol/1390_four_divisors.cpp
@@ -1,36 +1,37 @@
-/*Approach - first check if the number has 4 divisors and then return the sum of divisors.
-TC is O(m*sqrt(n))
+/*Approach - mathematically there are only two ways for a number to have 4 divisors.
+1. cube of a prime number
+2. product of two distinct prime numbers
+TC is O(m*n)
 SC is O(1)
 */
 class Solution {
 public:
-  bool fourdiv(int n) {
-    int count = 0;
-    for (int i = 1; i * i <= n; i++) {
+  bool isprime(int n) {
+    if (n < 2)
+      return false;
+    for (int i = 2; i * i <= n; i++) {
       if (n % i == 0)
-        count += 2;
-      if (i * i == n)
-        count--;
+        return false;
     }
-    return count == 4 ? true : false;
-  }
-  int sumfd(int n) {
-    int sum = 0;
-    for (int i = 1; i * i <= n; i++) {
-      if (n % i == 0) {
-        sum += i;
-        sum += n / i;
-      }
-      if (i * i == n)
-        sum -= i;
-    }
-    return sum;
+    return true;
   }
   int sumFourDivisors(vector<int> &nums) {
     int ans = 0;
-    for (int i : nums) {
-      if (fourdiv(i)) {
-        ans += sumfd(i);
+    int p, q;
+    for (int n : nums) {
+      p = cbrt(n);
+      if (p * p * p == n && isprime(p)) {
+        ans += 1 + p + p * p + p * p * p;
+      }
+      for (int j = 2; j * j <= n; j++) {
+        if (n % j == 0) {
+          p = j;
+          q = n / j;
+          if (p != q && isprime(p) && isprime(q)) {
+            ans += 1 + p + q + p * q;
+            break;
+          }
+        }
       }
     }
     return ans;
