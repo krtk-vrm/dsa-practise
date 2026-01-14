11d56648ea84372965cceaf627857d04860948f7
krtk_vrm <kartik2104verma@gmail.com>
2026-01-14 18:11:46 +0000

    Optimised the solution by using binary search to find the first negative element

--- questions-sol/1351_count_negative_numbers_in_a_sorted_matrix.cpp
+++ questions-sol/1351_count_negative_numbers_in_a_sorted_matrix.cpp
@@ -1,19 +1,23 @@
-/*Approach - Brute force checking every element;
-TC - O(n*m)
+/*Approach - Binary Search
+TC - O(n*logm)
 SC - O(1)
 */
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int count = 0;
-        int m = grid.size();
-        int n = grid[m-1].size();
-        for(int i = 0 ; i< m ; i++){
-            for(int j = n-1 ; j >=0; j--){
-                if(grid[i][j]<0)count++;
-                else break;
-            }
-        }
-        return count;
+  int countNegatives(vector<vector<int>> &grid) {
+    int count = 0;
+    int m = grid.size();
+    for (int i = 0; i < m; i++) {
+      int j = 0, k = grid[i].size() - 1;
+      while (j <= k) {
+        int mid = j + (k - j) / 2;
+        if (grid[i][mid] < 0) {
+          k = mid - 1;
+        } else
+          j = mid + 1;
+      }
+      count += grid[i].size() - j;
     }
+    return count;
+  }
 };
